c80fea0603221d5fcb762ea3d4692be77739390d
ice <meltedise@gmail.com>
2017-11-30 22:56:56 +0900

    configure redux store

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux'
+import createHistory from 'history/createBrowserHistory'
+
 import App from './containers/App'
+import createFinalStore from './store'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import './index.css'
+
+const history = createHistory()
+const store = createFinalStore(history)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+)
+
 registerServiceWorker()
