cadaf0908e7ac617ff6b7bf6349327525dde51ff
ice <meltedise@gmail.com>
2017-12-02 01:10:02 +0900

    fix App.test.js to work with withRouter

--- client/src/containers/App.test.js
+++ client/src/containers/App.test.js
@@ -1,8 +1,40 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux'
+
+import createHistory from 'history/createMemoryHistory'
+import { mount, configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+import createFinalStore from '../store'
 import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+configure({ adapter: new Adapter() })
+
+describe('App', () => {
+  let props
+  let mountedApp
+  const history = createHistory()
+  const store = createFinalStore(history)
+  const app = () => {
+    if (!mountedApp) {
+      mountedApp = mount(
+        <Provider store={store}>
+          <ConnectedRouter history={history}>
+            <App {...props} />
+          </ConnectedRouter>
+        </Provider>
+      )
+    }
+    return mountedApp
+  }
+
+  beforeEach(() => {
+    props = {}
+    mountedApp = undefined
+  })
+
+  it('renders without crashing', () => {
+    app()
+  })
 })
