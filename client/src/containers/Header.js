26105e9750cd88d8b99ae734306b184d32493471
ice <meltedise@gmail.com>
2017-12-02 17:16:08 +0900

    add header and a dummy navigation

--- client/src/containers/Header.js
+++ client/src/containers/Header.js
@@ -1,12 +1,26 @@
 import React from 'react'
-
-import logo from './logo.svg'
+import Navbar from 'react-bootstrap/lib/Navbar'
+import Nav from 'react-bootstrap/lib/Nav'
+import NavItem from 'react-bootstrap/lib/NavItem'
+import { Link } from 'react-router-dom'
+import { LinkContainer } from 'react-router-bootstrap'
 
 const Header = props => (
-  <header className='App-header'>
-    <img src={logo} className='App-logo' alt='logo' />
-    <h1 className='App-title'>Welcome to React</h1>
-  </header>
+  <Navbar inverse fixedTop>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <Link to='/'>Todorokite</Link>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav pullRight>
+        <LinkContainer to='/'>
+          <NavItem eventKey={1}>Dummy</NavItem>
+        </LinkContainer>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
 )
 
 export default Header
