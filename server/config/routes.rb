14b8eaf9528df97590ec9008e6d92ca1bb152488
ice <meltedise@gmail.com>
2017-12-07 00:52:44 +0900

    fix routing spec

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :v1 do
-    resources :items
+    resources :items, defaults: { format: 'json' }
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
