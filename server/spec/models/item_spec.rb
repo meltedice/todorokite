10f6637f9b9ff1af0971a86f3fd8df9cb3758e9d
ice <meltedise@gmail.com>
2017-12-07 00:58:07 +0900

    add state 'active' and 'done'

--- server/spec/models/item_spec.rb
+++ server/spec/models/item_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context '.save' do
+    it 'saves with name and note' do
+      item = Item.new(name: 'item name', note: 'item note')
+      item.save!
+      expect(item).to be_persisted
+      expect(item.state).to eq 'active'
+    end
+  end
+
+  context 'with complete event' do
+    it 'changes state to done' do
+      item = Item.new(name: 'item name', note: 'item note')
+      item.complete
+      expect(item.state).to eq 'done'
+    end
+  end
+
+  context 'with uncomplete event' do
+    it 'changes state to active' do
+      item = Item.new(name: 'item name', note: 'item note')
+      item.complete
+      expect(item.state).to eq 'done'
+      item.uncomplete
+      expect(item.state).to eq 'active'
+    end
+  end
 end
