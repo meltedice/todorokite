af2ed913b3e4b73af209b48b508113482232a480
ice <meltedise@gmail.com>
2017-12-08 20:47:07 +0900

    add test cases for Item's active/completed state

--- server/spec/models/item_spec.rb
+++ server/spec/models/item_spec.rb
@@ -5,8 +5,10 @@ RSpec.describe Item, type: :model do
     it 'saves with name and note' do
       item = Item.new(name: 'item name', note: 'item note')
       item.save!
+
       expect(item).to be_persisted
       expect(item.state).to eq 'active'
+      expect(item).to be_active
     end
   end
 
@@ -14,17 +16,22 @@ RSpec.describe Item, type: :model do
     it 'changes state to completed' do
       item = Item.new(name: 'item name', note: 'item note')
       item.complete
+
       expect(item.state).to eq 'completed'
+      expect(item).to be_completed
     end
   end
 
   context 'with uncomplete event' do
     it 'changes state to active' do
       item = Item.new(name: 'item name', note: 'item note')
-      item.complete
-      expect(item.state).to eq 'completed'
+      expect(item).to be_active
+      item.complete!
+
+      expect(item).to be_completed
+
       item.uncomplete
-      expect(item.state).to eq 'active'
+      expect(item).to be_active
     end
   end
 end
