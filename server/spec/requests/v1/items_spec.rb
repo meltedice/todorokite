0ccd76227efda983441e01d44107b0a1e8768590
ice <meltedise@gmail.com>
2017-12-09 22:37:08 +0900

    add request specs for index and delete methods

--- server/spec/requests/v1/items_spec.rb
+++ server/spec/requests/v1/items_spec.rb
@@ -1,10 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "Items", type: :request do
+  before(:each) do
+    @items = 5.times.map { create(:item) }
+  end
+
   describe "GET /v1/items" do
-    it "works! (now write some real specs)" do
+    it "returns 5 items" do
       get v1_items_path(format: :json)
       expect(response).to have_http_status(200)
+      json = JSON.parse(response.body)
+      expect(json.size).to eq(5)
     end
   end
+
+  describe "DELETE /v1/items" do
+    it "deletes 1 item" do
+      item = @items[2]
+      expect(Item.count).to eq(5)
+
+      delete v1_item_path(item.id, format: :json)
+      expect(response).to have_http_status(204)
+
+      expect(Item.count).to eq(4)
+    end
+  end
+
 end
